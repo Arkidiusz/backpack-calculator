35449c2019a1529115dc51428cdbd1d6a56e27a7
Arkadiusz Kupracz <arkadiusz.kupracz@gmail.com>
2024-01-05 22:11:43 +0000

    feat: add Backpack and Item classes (#12)
    * feat: add Backpack and Item classes
    
    * fix: fix type hints
    
    ---------
    
    Co-authored-by: Arkadiusz Kupracz <arkadiusz.kupracz.com>

--- src/main.py
+++ src/main.py
@@ -1,5 +1,25 @@
+class Item:
+    """Item represents all properties of an item used to evaluate its value contribution to a backpack
+
+    Attributes:
+        name: name of item
+        tags: A list of tags of item such as "food" or "bag"
+    """
+    def __init__(cls, name: str, tags: list[str]) -> None:
+        cls.name = name
+        cls.tags = tags
+
 def add_numbers(a, b):
+    "This is just for testing - to be removed"
+    
     print("running main.py")
     return a + b
 
-print(add_numbers(1, 2))
+class Backpack:
+    """Represents all information in a backpack used to calculate its value
+
+    Attributes:
+        items: a list of items in a backpack
+    """
+    def __init__(cls, items: list[Item] = []) -> None:
+        cls.items = items
