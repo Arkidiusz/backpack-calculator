f4c5aec463c96dd3be603194e22486e815abc429
Arkadiusz Kupracz <arkadiusz.kupracz.com>
2024-01-05 22:10:50 +0000

    fix: fix type hints

--- src/main.py
+++ src/main.py
@@ -1,12 +1,3 @@
-class Backpack:
-    """Represents all information in a backpack used to calculate its value
-
-    Attributes:
-        items: a list of items in a backpack
-    """
-    def __init__(cls, items: List[Item] = []) -> None:
-        cls.items = items
-
 class Item:
     """Item represents all properties of an item used to evaluate its value contribution to a backpack
 
@@ -14,10 +5,21 @@ class Item:
         name: name of item
         tags: A list of tags of item such as "food" or "bag"
     """
-    def __init__(cls, name: str, tags: List[str]) -> None:
+    def __init__(cls, name: str, tags: list[str]) -> None:
         cls.name = name
         cls.tags = tags
 
 def add_numbers(a, b):
+    "This is just for testing - to be removed"
+    
     print("running main.py")
     return a + b
+
+class Backpack:
+    """Represents all information in a backpack used to calculate its value
+
+    Attributes:
+        items: a list of items in a backpack
+    """
+    def __init__(cls, items: list[Item] = []) -> None:
+        cls.items = items
