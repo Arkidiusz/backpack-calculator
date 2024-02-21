f9952593b9112e992ec74935cce17a87b60480e5
Arkadiusz Kupracz <arkadiusz.kupracz.com>
2024-02-21 16:18:36 +0000

    feat: add expected hits

--- src/controller.py
+++ src/controller.py
@@ -5,6 +5,7 @@ import src.items
 
 
 combat_duration = 16
+expected_hits = 10
 
 def get_item_data():
     item_data_file = open('data/items.json', encoding='utf-8')
@@ -12,3 +13,6 @@ def get_item_data():
 
 def get_combat_duration():
     return combat_duration
+
+def get_expected_hits():
+    return expected_hits
