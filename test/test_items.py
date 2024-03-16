ffbfd7278495c7a2b136a8a61264fa2a9a2cb1da
Arkadiusz Kupracz <arkadiusz.kupracz.com>
2024-03-16 22:08:22 +0000

    chore: polish test_items.py

--- test/test_items.py
+++ test/test_items.py
@@ -3,17 +3,21 @@ from src.config import get_item_data
 
 import pytest
 
+
 @pytest.fixture
 def item_name():
-    return 'Wooden Sword'
+    return "Wooden Sword"
+
 
 @pytest.fixture
 def item_tags():
-    return ['Melee Weapon']
+    return ["Melee Weapon"]
+
 
 @pytest.fixture
 def item_cost():
     return 3
+
 
 def test_create_item(item_name, item_tags, item_cost):
     # Act
@@ -24,102 +28,111 @@ def test_create_item(item_name, item_tags, item_cost):
     assert item.tags == item_tags
     assert item.cost == item_cost
 
+
 def test_get_metrics_raises(item_name):
     # Except
     item = Item(item_name)
     with pytest.raises(BackpackException):
         item.get_metrics()
 
+
 def test_create_banana():
     # Arrange
-    attributes = get_item_data()['items']['Banana']['attributes']
+    attributes = get_item_data()["items"]["Banana"]["attributes"]
 
     # Act
     banana = Banana()
 
     # Assert
-    banana.heal = attributes['heal']
-    banana.stamina_regeneration = attributes['stamina_regeneration']
-    banana.cooldown = attributes['cooldown']
+    banana.heal = attributes["heal"]
+    banana.stamina_regeneration = attributes["stamina_regeneration"]
+    banana.cooldown = attributes["cooldown"]
+
 
 def test_banana_get_metrics():
     # Arrange
-    attributes = get_item_data()['items']['Banana']['attributes']
+    attributes = get_item_data()["items"]["Banana"]["attributes"]
 
     # Act
     banana = Banana()
     metrics = banana.get_metrics()
 
     # Assert
-    assert metrics['healing'] == 12
-    assert metrics['stamina'] == 3
+    assert metrics["healing"] == 12
+    assert metrics["stamina"] == 3
+
 
 def test_create_garlic():
     # Arrange
-    attributes = get_item_data()['items']['Garlic']['attributes']
+    attributes = get_item_data()["items"]["Garlic"]["attributes"]
 
     # Act
     garlic = Garlic()
 
     # Assert
-    garlic.armor_generation = attributes['armor_generation']
-    garlic.vampirism_removal = attributes['vampirism_removal']
-    garlic.vampirism_removal_chance = attributes['vampirism_removal_chance']
+    garlic.armor_generation = attributes["armor_generation"]
+    garlic.vampirism_removal = attributes["vampirism_removal"]
+    garlic.vampirism_removal_chance = attributes["vampirism_removal_chance"]
+
 
 def test_garlic_get_metrics():
     # Arrange
-    attributes = get_item_data()['items']['Garlic']['attributes']
+    attributes = get_item_data()["items"]["Garlic"]["attributes"]
 
     # Act
     garlic = Garlic()
     metrics = garlic.get_metrics()
 
     # Assert
-    assert metrics['armor'] == 12
-    assert metrics['vampirism_removal'] == 1.2
+    assert metrics["armor"] == 12
+    assert metrics["vampirism_removal"] == 1.2
+
 
 def test_create_wooden_sword():
     # Arrange
-    attributes = get_item_data()['items']['Wooden Sword']['attributes']
+    attributes = get_item_data()["items"]["Wooden Sword"]["attributes"]
 
     # Act
     sword = WoodenSword()
 
     # Assert
-    assert sword.minimum_damage == attributes['minimum_damage']
-    assert sword.maximum_damage == attributes['maximum_damage']
-    assert sword.cooldown == attributes['cooldown']
-    assert sword.accuracy == attributes['accuracy']
-    assert sword.stamina_cost == attributes['stamina_cost']
+    assert sword.minimum_damage == attributes["minimum_damage"]
+    assert sword.maximum_damage == attributes["maximum_damage"]
+    assert sword.cooldown == attributes["cooldown"]
+    assert sword.accuracy == attributes["accuracy"]
+    assert sword.stamina_cost == attributes["stamina_cost"]
+
 
 def test_wooden_sword_get_metrics():
     # Arrange
-    attributes = get_item_data()['items']['Wooden Sword']['attributes']
+    attributes = get_item_data()["items"]["Wooden Sword"]["attributes"]
 
     # Act
     sword = WoodenSword()
     metrics = sword.get_metrics()
 
     # Assert
-    assert metrics['damage'] == 19.8
-    assert round(metrics['stamina_cost'], 2) == 7.7
+    assert metrics["damage"] == 19.8
+    assert round(metrics["stamina_cost"], 2) == 7.7
+
 
 def test_create_pan():
     # Arrange
-    attributes = get_item_data()['items']['Pan']['attributes']
+    attributes = get_item_data()["items"]["Pan"]["attributes"]
     adjacent_foods = 4
 
     # Act
     pan = Pan(adjacent_foods)
 
     # Assert
-    assert pan.minimum_damage == attributes['minimum_damage']
-    assert pan.maximum_damage == attributes['maximum_damage']
-    assert pan.cooldown == attributes['cooldown']
-    assert pan.accuracy == attributes['accuracy']
-    assert pan.stamina_cost == attributes['stamina_cost']
-    assert pan.damage_bonus == attributes['damage_bonus']
+    assert pan.minimum_damage == attributes["minimum_damage"]
+    assert pan.maximum_damage == attributes["maximum_damage"]
+    assert pan.cooldown == attributes["cooldown"]
+    assert pan.accuracy == attributes["accuracy"]
+    assert pan.stamina_cost == attributes["stamina_cost"]
+    assert pan.damage_bonus == attributes["damage_bonus"]
     assert pan.adjacent_foods == adjacent_foods
+
 
 def test_pan_get_metrics():
     # Arrange
@@ -130,25 +143,27 @@ def test_pan_get_metrics():
     metrics = pan.get_metrics()
 
     # Assert
-    assert metrics['damage'] == 32.725
-    assert metrics['stamina_cost'] == 6.3
+    assert metrics["damage"] == 32.725
+    assert metrics["stamina_cost"] == 6.3
+
 
 def test_create_stone():
     # Arrange
-    attributes = get_item_data()['items']['Stone']['attributes']
+    attributes = get_item_data()["items"]["Stone"]["attributes"]
     bag_of_marbles = True
 
     # Act
     stone = Stone(bag_of_marbles)
 
     # Assert
-    assert stone.minimum_damage == attributes['minimum_damage']
-    assert stone.maximum_damage == attributes['maximum_damage']
-    assert stone.cooldown == attributes['cooldown']
-    assert stone.accuracy == attributes['accuracy']
-    assert stone.stamina_cost == attributes['stamina_cost']
-    assert stone.armor_destruction == attributes['armor_destruction']
+    assert stone.minimum_damage == attributes["minimum_damage"]
+    assert stone.maximum_damage == attributes["maximum_damage"]
+    assert stone.cooldown == attributes["cooldown"]
+    assert stone.accuracy == attributes["accuracy"]
+    assert stone.stamina_cost == attributes["stamina_cost"]
+    assert stone.armor_destruction == attributes["armor_destruction"]
     assert stone.bag_of_marbles == bag_of_marbles
+
 
 def test_stone_get_metrics():
     # Arrange
@@ -159,19 +174,21 @@ def test_stone_get_metrics():
     metrics = stone.get_metrics()
 
     # Assert
-    assert round(metrics['damage'] , 2) == 1.95
-    assert metrics['stamina_cost'] == 0
-    assert round(metrics['armor_destruction'] , 2) == 1.95
+    assert round(metrics["damage"], 2) == 1.95
+    assert metrics["stamina_cost"] == 0
+    assert round(metrics["armor_destruction"], 2) == 1.95
+
 
 def test_create_healing_herbs():
     # Arrange
-    attributes = get_item_data()['items']['Healing Herbs']['attributes']
+    attributes = get_item_data()["items"]["Healing Herbs"]["attributes"]
 
     # Act
     healing_herbs = HealingHerbs()
 
     # Assert
-    assert healing_herbs.regeneration == attributes['regeneration']
+    assert healing_herbs.regeneration == attributes["regeneration"]
+
 
 def test_healing_herbs_get_metrics():
     # Act
@@ -179,17 +196,19 @@ def test_healing_herbs_get_metrics():
     metrics = stone.get_metrics()
 
     # Assert
-    assert metrics['regeneration'] == 2
+    assert metrics["regeneration"] == 2
+
 
 def test_create_healing_herbs():
     # Arrange
-    attributes = get_item_data()['items']['Pocket Sand']['attributes']
+    attributes = get_item_data()["items"]["Pocket Sand"]["attributes"]
 
     # Act
     pocket_sand = PocketSand()
 
     # Assert
-    assert pocket_sand.blind== attributes['blind']
+    assert pocket_sand.blind == attributes["blind"]
+
 
 def test_healing_herbs_get_metrics():
     # Act
@@ -197,19 +216,21 @@ def test_healing_herbs_get_metrics():
     metrics = pocket_sand.get_metrics()
 
     # Assert
-    assert metrics['blind'] == 1
+    assert metrics["blind"] == 1
+
 
 def test_create_wooden_buckler():
     # Arrange
-    attributes = get_item_data()['items']['Wooden Buckler']['attributes']
+    attributes = get_item_data()["items"]["Wooden Buckler"]["attributes"]
 
     # Act
     wooden_buckler = WoodenBuckler()
 
     # Assert
-    assert wooden_buckler.proc_chance == attributes['proc_chance']
-    assert wooden_buckler.damage_absorption== attributes['damage_absorption']
-    assert wooden_buckler.stamina_removal == attributes['stamina_removal']
+    assert wooden_buckler.proc_chance == attributes["proc_chance"]
+    assert wooden_buckler.damage_absorption == attributes["damage_absorption"]
+    assert wooden_buckler.stamina_removal == attributes["stamina_removal"]
+
 
 def test_healing_herbs_get_metrics():
     # Act
@@ -217,18 +238,20 @@ def test_healing_herbs_get_metrics():
     metrics = wooden_buckler.get_metrics()
 
     # Assert
-    assert metrics['damage_absorption'] == 14.0
-    assert metrics['stamina_damage'] == 1.05
+    assert metrics["damage_absorption"] == 14.0
+    assert metrics["stamina_damage"] == 1.05
+
 
 def test_create_walrus_tusk():
     # Arrange
-    attributes = get_item_data()['items']['Walrus Tusk']['attributes']
+    attributes = get_item_data()["items"]["Walrus Tusk"]["attributes"]
 
     # Act
     walrus_tusk = WalrusTusk()
 
     # Assert
-    assert walrus_tusk.spikes == attributes['spikes']
+    assert walrus_tusk.spikes == attributes["spikes"]
+
 
 def test_walrus_tusk_get_metrics():
     # Act
@@ -236,4 +259,4 @@ def test_walrus_tusk_get_metrics():
     metrics = walrus_tusk.get_metrics()
 
     # Assert
-    assert metrics['damage'] == 10
+    assert metrics["damage"] == 10
--- test/test_main_window.py
+++ test/test_main_window.py
@@ -8,69 +8,80 @@ import src.controller as controller
 
 import re
 
+
 @pytest.fixture
 def backpack():
     return Backpack()
+
 
 @pytest.fixture
 def main_window(qtbot):
     return MainWindow()
 
+
 def test_populate_metrics_table(backpack, main_window):
     # Arrange
     metrics = backpack.compute_metrics()
-    
+
     # Act
     main_window.populate_metrics_table(metrics)
-    
+
     # Assert
     labels = main_window.grid_container.findChildren(QLabel)
     assert len(labels) == len(metrics) * 2
     for row in range(main_window.metrics_grid.rowCount()):
-        metric_name = re.sub(':', '', main_window.metrics_grid.itemAtPosition(row, 0).widget().text())
-        metric_value = float(main_window.metrics_grid.itemAtPosition(row, 1).widget().text())
-        assert f'{metric_name}' in metrics.keys()
+        metric_name = re.sub(
+            ":", "", main_window.metrics_grid.itemAtPosition(row, 0).widget().text()
+        )
+        metric_value = float(
+            main_window.metrics_grid.itemAtPosition(row, 1).widget().text()
+        )
+        assert f"{metric_name}" in metrics.keys()
         assert metrics[metric_name] == metric_value
+
 
 def test_add_item_button_clicked(qtbot):
     # Arrange
     mock_open_popup = MagicMock()
-    
+
     # Act
-    with patch('src.main_window.MainWindow._open_popup') as mock_open_popup:
+    with patch("src.main_window.MainWindow._open_popup") as mock_open_popup:
         main_window = MainWindow()
         qtbot.mouseClick(main_window.add_item_button, Qt.LeftButton)
-        
+
     # Assert
     mock_open_popup.assert_called_once()
+
 
 def test_ok_button_clicked(qtbot):
     # Arrange
     mock_add_items = MagicMock()
     mock_populate_metrics_table = MagicMock()
-    with patch('src.main_window.add_item', autospec = True) as mock_add_items:
+    with patch("src.main_window.add_item", autospec=True) as mock_add_items:
         main_window = MainWindow()
         main_window.populate_metrics_table = mock_populate_metrics_table
         add_item_popup = AddItemPopup(main_window)
-        add_item_popup.items_combo_box.setCurrentText('Wooden Sword')
-        
+        add_item_popup.items_combo_box.setCurrentText("Wooden Sword")
+
         # Act
         qtbot.mouseClick(add_item_popup.ok_button, Qt.LeftButton)
-    
+
     # Assert
-    mock_add_items.assert_called_with('Wooden Sword')
+    mock_add_items.assert_called_with("Wooden Sword")
     mock_populate_metrics_table.assert_called_once()
     assert main_window.item_list_widget.count() == 1
+
 
 def test_items_combo_box(main_window):
     # Arrange
     add_item_popup = AddItemPopup(main_window)
-    
-    #Assert
+
+    # Assert
     items = get_item_names()
     for i in range(len(items)):
         assert items[i] == add_item_popup.items_combo_box.itemText(i)
-    
+
+
 def test_delete_item(main_window, qtbot):
     # Arrange
     add_item_popup = AddItemPopup(main_window)
@@ -79,11 +90,10 @@ def test_delete_item(main_window, qtbot):
     main_window.populate_metrics_table = populate_metrics_mock
     items = main_window.item_list_widget.findItems("Banana", Qt.MatchFixedString)
     items[0].setSelected(True)
-    
+
     # Act
     qtbot.mouseClick(main_window.delete_item_button, Qt.LeftButton)
 
     # Assert
     assert controller.backpack.items == {}
     populate_metrics_mock.assert_called_once()
-    
