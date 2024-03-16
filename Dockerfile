a1062bc67eb019cf39e2e5b4931cbc540fdf8ece
Arkadiusz Kupracz <arkadiusz.kupracz@gmail.com>
2024-03-16 02:20:26 +0000

    46 handle item addition (#77)
    * feat: place metrics table in horizontal layout
    
    * feat: add button for adding items
    
    * feat: add ComboBox for items
    
    * feat: populate items comboBox with real data
    
    * feat: remove WindowContextHelpButtonHint
    
    * feat: update metric on item addition
    
    * chore: make gui code more readable
    
    * test: add test_controller.py
    
    * test: add test_add_item_calls_open_popup
    
    * refactor: add separate class for popup window
    
    * test: add test_ok_button_clicked
    
    * test: add test_items_combo_box
    
    ---------
    
    Co-authored-by: Arkadiusz Kupracz <arkadiusz.kupracz.com>

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM jenkins/jenkins
 
 USER root
 RUN apt-get update && \
-    apt-get install -y python3 python3-pip python3-venv libglib2.0-0 libsm6 libxrender1 libxext6 && \
+    apt-get install -y python3 python3-pip python3-venv libglib2.0-0 libsm6 libxrender1 libxext6 xvfb && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 USER jenkins
--- src/main_window.py
+++ src/main_window.py
@@ -1,46 +1,56 @@
-from PyQt5.QtWidgets import QMainWindow, QScrollArea, QVBoxLayout, QWidget, QLabel, QSizePolicy, QGridLayout
+from PyQt5.QtWidgets import QMainWindow, QScrollArea, QVBoxLayout, QHBoxLayout, QWidget, QLabel, QSizePolicy, QGridLayout, QPushButton, QDialog, QComboBox
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtCore import Qt
 
-from .controller import request_metrics_update
+from src.controller import add_item, request_metrics_update
+from .config import get_item_names
 
 class MainWindow(QMainWindow):
     """Defines the appearance and elements of the application
     """
     def __init__(self):
+        """Initialise main window, its central widget, layout and call functions to create all widgets and components of this window
+        """
         super().__init__()
 
         self.setWindowTitle("Backpack Calculator")
         screen_resolution = QGuiApplication.primaryScreen().availableGeometry()
         self.setGeometry(0, 0, screen_resolution.width(), screen_resolution.height())
 
-        self.central_widget = QWidget()
-        self.setCentralWidget(self.central_widget)
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+        self.central_layout = QHBoxLayout()
+        self.central_layout.setAlignment(Qt.AlignLeft)
+        central_widget.setLayout(self.central_layout)
 
         self._create_metrics_table()
-        self.populate_metrics_table(request_metrics_update())
+        self._create_add_item_button()
     
     def _create_metrics_table(self) -> None:
         """Creates a display column of metrics
         """
         layout = QVBoxLayout()
         layout.setAlignment(Qt.AlignTop)
-        self.central_widget.setLayout(layout)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.central_layout.addWidget(widget)
         
         metrics_label = QLabel('Metrics:')
-
-        scroll_area = QScrollArea()
-        scroll_area.setWidgetResizable(True)
-        scroll_area.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
 
         self.grid_container = QWidget()
         self.metrics_grid = QGridLayout(self.grid_container)
         self.metrics_grid.setAlignment(Qt.AlignTop)
         self.grid_container.setLayout(self.metrics_grid)
+
+        scroll_area = QScrollArea()
+        scroll_area.setWidgetResizable(True)
+        scroll_area.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Expanding)
         scroll_area.setWidget(self.grid_container)
 
         layout.addWidget(metrics_label)
         layout.addWidget(scroll_area)
+
+        self.populate_metrics_table(request_metrics_update())
     
     def populate_metrics_table(self, metrics: dict[str, float]) -> None:
         """Updates the metrics table with provided metrics
@@ -61,3 +71,44 @@ class MainWindow(QMainWindow):
             self.metrics_grid.addWidget(metric_value_label, row, 1)
 
             row += 1
+
+    def _create_add_item_button(self):
+        """ A button for adding new items to the backpack
+        """
+        self.add_item_button = QPushButton("Add Item")
+        self.add_item_button.clicked.connect(self._open_popup)
+        self.central_layout.addWidget(self.add_item_button)   
+    
+    def _open_popup(self):
+        """ A popup window enabling item selection
+        """
+        popup = AddItemPopup(self)
+        popup.exec_()
+
+
+class AddItemPopup(QDialog):
+    def __init__(self, main_window: MainWindow):
+        super().__init__()
+        
+        self.main_window = main_window
+        
+        self.setWindowTitle("Add Item")
+        self.setWindowFlag(Qt.WindowContextHelpButtonHint, False)
+        self.accepted.connect(self._add_item)
+        
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.items_combo_box = QComboBox()
+        self.items_combo_box.addItems(get_item_names())
+        
+        self.ok_button = QPushButton("Add Item")
+        self.ok_button.clicked.connect(self.accept)
+
+        layout.addWidget(self.items_combo_box)
+        layout.addWidget(self.ok_button)
+    
+    def _add_item(self) -> None:
+        metrics = add_item(self.items_combo_box.currentText())
+        self.main_window.populate_metrics_table(metrics)
+        
