56e108da7f5a80e22d06e1ddc45c8aea6b7c3c37
Arkadiusz Kupracz <arkadiusz.kupracz.com>
2024-03-16 22:27:33 +0000

    chore: polish controller.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,3 +1,2 @@
 class BackpackException(Exception):
-    """ A base class for all exceptions related to Backpack errors
-    """
+    """A base class for all exceptions related to Backpack errors"""
