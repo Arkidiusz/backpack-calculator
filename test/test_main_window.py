d9eb38124ddaa265ea721ec415f3e7585c844a5e
Arkadiusz Kupracz <arkadiusz.kupracz.com>
2024-03-17 22:30:06 +0000

    test: add test_update_metrics

--- test/test_main_window.py
+++ test/test_main_window.py
@@ -19,6 +19,11 @@ def main_window(qtbot):
     return MainWindow()
 
 
+@pytest.fixture
+def combat_duration():
+    return controller.get_combat_duration() + 1
+
+
 def test_populate_metrics_table(backpack, main_window):
     # Arrange
     metrics = backpack.compute_metrics()
@@ -98,16 +103,35 @@ def test_delete_item(main_window, qtbot):
     assert controller.backpack.items == {}
     populate_metrics_mock.assert_called_once()
 
-def test_click_combat_duration_spinner(qtbot):
+
+def test_click_combat_duration_spinner(qtbot, combat_duration):
     # Arrange
-    combat_duration = controller.get_combat_duration() + 1
-    with patch("src.main_window.MainWindow._update_combat_duration", autospec=False) as mock_update_combat_duration:
+    # combat_duration = controller.get_combat_duration() + 1
+    with patch(
+        "src.main_window.MainWindow._update_combat_duration", autospec=False
+    ) as mock_update_combat_duration:
         main_window = MainWindow()
         spinner = main_window.combat_duration_spinner
-        
+
         # Act
         spinner.setValue(combat_duration)
-        
+
     # Assert
     assert spinner.value() == combat_duration
     mock_update_combat_duration.assert_called_with(combat_duration)
+
+
+@patch("src.main_window.MainWindow.populate_metrics_table")
+@patch("src.main_window.set_combat_duration")
+def test_update_combat_duration(
+    set_combat_duration_mock, populate_metrics_table_mock, combat_duration
+):
+    # Arrange
+    main_window = MainWindow()
+
+    # Act
+    main_window._update_combat_duration(combat_duration)
+
+    # Assert
+    set_combat_duration_mock.assert_called_with(combat_duration)
+    assert populate_metrics_table_mock.call_count == 2
